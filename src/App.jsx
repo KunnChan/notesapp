0b8a81a69e96c8821dcbde1b1a588e6239e7e1d0
Min Kunn Chan <kunnchan3@gmail.com>
2025-04-19 09:07:44 +0800

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello from Amplify with Auto build.
         </p>
       </div>
       <p className="read-the-docs">
